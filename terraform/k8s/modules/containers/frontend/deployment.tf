8bdc00032975f3c88a9648871ff0571716d808d2
Grant Birkinbine <grantbirki@github.com>
2021-09-13 20:55:36 -0700

    another typo

--- terraform/k8s/modules/containers/frontend/deployment.tf
+++ terraform/k8s/modules/containers/frontend/deployment.tf
@@ -1,14 +1,14 @@
-data "kubectl_file_documents" "backend_deployment_manifest" {
+data "kubectl_file_documents" "frontend_deployment_manifest" {
   depends_on = [
-    data.kubectl_file_documents.backend_namespace_manifest
+    data.kubectl_file_documents.frontend_namespace_manifest
   ]
-  content = file("modules/containers/backend/deployment.yaml")
+  content = file("modules/containers/frontend/deployment.yaml")
 }
 
-resource "kubectl_manifest" "backend_deployment" {
+resource "kubectl_manifest" "frontend_deployment" {
   depends_on = [
-    data.kubectl_file_documents.backend_namespace_manifest
+    data.kubectl_file_documents.frontend_namespace_manifest
   ]
-  count     = length(data.kubectl_file_documents.backend_deployment_manifest.documents)
-  yaml_body = element(data.kubectl_file_documents.backend_deployment_manifest.documents, count.index)
+  count     = length(data.kubectl_file_documents.frontend_deployment_manifest.documents)
+  yaml_body = element(data.kubectl_file_documents.frontend_deployment_manifest.documents, count.index)
 }
