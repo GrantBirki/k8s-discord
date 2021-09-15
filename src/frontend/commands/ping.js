182a5246444ef61f80d695a76f56ff2f43895a19
Grant Birkinbine <grantbirki@github.com>
2021-09-14 19:40:53 -0700

    replace fronthealth with ping

--- src/frontend/commands/ping.js
+++ src/frontend/commands/ping.js
@@ -1,15 +1,16 @@
+// This function is useful for checking the frontend container health
 // Import SlashCommandBuild to handle slash commands
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 // Export the module which handles the slash command
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('fronthealth') // The name of the Discord Slash command
-        .setDescription('Returns the healthcheck of the frontend'), // The description of the Discord Slash command
+        .setName('ping') // The name of the Discord Slash command
+        .setDescription('Returns pong'), // The description of the Discord Slash command
 
     // The function to execute when the slash command is called (calls our backend)
     async execute(interaction) {
         // Sends a reply to the Slash command which triggered this function
-        interaction.reply({ content: "I'm up!" });
+        interaction.reply({ content: "pong" });
     }
 };
