c2425aa07ebff616aa879b8afc44d7de71213299
Grant Birkinbine <grantbirki@github.com>
2021-09-29 16:01:48 -0700

    k8s-cluster updates

--- terraform/k8s-cluster/acr.tf
+++ terraform/k8s-cluster/acr.tf
@@ -1,5 +1,5 @@
 data "azurerm_container_registry" "acr" {
-  name                = "${var.project_name}k8sacr" # Did you get an error saying that your repo must be globally unique? Try adding some extra charcters here
+  name                = "${var.PROJECT_NAME}k8sacr${var.ENVIRONMENT}" # Did you get an error saying that your repo must be globally unique? Try adding some extra charcters here
   resource_group_name = azurerm_resource_group.default.name
 
   depends_on = [
@@ -8,12 +8,12 @@ data "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.project_name}k8sacr"
+  name                = "${var.PROJECT_NAME}k8sacr${var.ENVIRONMENT}"
   resource_group_name = azurerm_resource_group.default.name
-  location            = var.cloud_location
+  location            = var.CLOUD_LOCATION
   sku                 = "Basic"
   admin_enabled       = true
-  # georeplication_locations = ["${var.cloud_location}"]
+  # georeplication_locations = ["${var.CLOUD_LOCATION}"]
   # retention_policy {
   #   enabled = true
   # }
